2581c23b823e91a488992309ca0a7a36406bf1d2
Enyerbe Davila <enyerbe.davilalopez@gmail.com>
2022-04-29 11:27:11 -0300

    Segunda Entrega del proyecto Final

--- routes/routerProductos.js
+++ routes/routerProductos.js
@@ -5,14 +5,16 @@ const routerProductos = express.Router()
 routerProductos.use(express.urlencoded({ extended: true }))
 routerProductos.use(express.json())
 
+// un get que devuelve todos los productos.
 routerProductos.get("/", (req, res) => {
     getting(req, res)
 })
 
+// un get que devielve un producto por su id.
 routerProductos.get("/:id", (req, res) => {
     gettingById(req, res)
 })
-
+// un post que require que le envies un objeto a travez del body con: nombre, descripcion, precio, urlfoto y stock.
 routerProductos.post("/", (req, res, next) => {
     if (req.query.admin == "true") {
         next()
@@ -22,7 +24,7 @@ routerProductos.post("/", (req, res, next) => {
 }, (req, res) => {
     postProducto(req, res)
 })
-
+// un put que require que le envies el id del objeto a actualizar y un objeto a traves del body con los parametros a actualizar; ejemplo: {stock: 20}.
 routerProductos.put("/:id", (req, res, next) => {
     if (req.query.admin == "true") {
         next()
@@ -32,7 +34,7 @@ routerProductos.put("/:id", (req, res, next) => {
 }, (req, res) => {
     reemplazar(req, res)
 })
-
+// permite borrar un objeto por su id.
 routerProductos.delete("/:id", (req, res, next) => {
     if (req.query.admin == "true") {
         next()
