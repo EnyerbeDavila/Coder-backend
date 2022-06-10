71cbf691678ba9c7fb88e96c63824f405e427f3c
Enyerbe Davila <enyerbe.davilalopez@gmail.com>
2022-06-10 02:46:42 -0300

    Desafio Clusters

---
+++ Daos/dao.js
@@ -0,0 +1,87 @@
+const optionsMini = { default: { puerto: '8080', modo: 'FORK' } }
+const parseArgs = require('minimist')
+const args = parseArgs(process.argv, optionsMini)
+const { fork } = require('child_process')
+const { faker } = require('@faker-js/faker')
+faker.locale = 'es'
+const os = require('os')
+
+
+module.exports = {
+    RenderFallo: (res) => {
+        res.render('login-error.ejs')
+    },
+    RenderFallo2: (res) => {
+        res.render('Regis-error.ejs')
+    },
+    login: (req, res) => {
+        req.logOut()
+        res.render('formularioLog.ejs')
+    },
+    RenderRegis: (res) => {
+        res.render('formularioRegist.ejs')
+    },
+    RenderLogout: (req, res) => {
+        let user = req.user.username
+        res.render('Bye.ejs', { user: user })
+    },
+    RenderPrinc: (req, res) => {
+        let user = req.user.username
+        res.render('index.ejs', { nombre: user })
+    },
+    SendInfo: (res) => {
+        let objeto = {
+            ArgumentosDeEntrada: args,
+            SistemaOperativo: os.type(),
+            VersionNodejs: process.version,
+            Rss: process.memoryUsage.rss(),
+            PathEjecucion: process.execPath,
+            PathEjecutable: args._[1],
+            processId: process.pid,
+            NumerosDeNucleos: os.cpus().length
+        }
+        res.send(objeto)
+    },
+    NumberRandoms: (req, res) => {
+        let cant = 0
+        let numeros = []
+        let objNumeros = []
+        if (req.query.cant == undefined) {
+            cant = 100000000
+        } else {
+            cant = req.query.cant
+        } for (let i = 0; i < cant; i++) {
+            let numero = Math.floor(Math.random() * (1000 - 1) + 1)
+            numeros.push(numero)
+        }
+        let contador = 0
+        let indice
+        for (let i = 1; i < 1000;) {
+            indice = numeros.indexOf(i)
+            if (indice != -1) {
+                contador++
+                numeros.splice(indice, 1)
+            } else {
+                registrar(i, contador)
+                i++
+                contador = 0
+            }
+        }
+        function registrar(numero, veces) {
+            let obj = `{${numero} : ${veces}}`
+            objNumeros.push(obj)
+        }
+        res.send(objNumeros)
+    },
+    ProductosTest: (res) => {
+        let productos = []
+        for (let i = 0; i < 5; i++) {
+            let producto = {}
+            producto.Nombre = faker.commerce.product()
+            producto.Precio = faker.commerce.price()
+            producto.UrlFoto = faker.image.imageUrl()
+            productos.push(producto)
+        }
+        res.send(JSON.stringify(productos))
+    }
+}
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const util = require('util')
 const mongoose = require("mongoose")
 const modelPost = require("./models/schemaPost")
 const modelUser = require('./models/schemaUser')
-const { faker } = require('@faker-js/faker')
-faker.locale = 'es'
 const normalizr = require('normalizr')
 const { Server: HttpServer } = require('http')
 const { Server: IOServer } = require('socket.io')
@@ -18,251 +16,245 @@ const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 const config = require('./config')
 const parseArgs = require('minimist')
-const { fork } = require('child_process')
 const os = require('os')
+const cluster = require('cluster')
 
-async function CRUD() {
-  try {
-    const URL = "mongodb://127.0.0.1:27017/post"
-    let rta = await mongoose.connect(URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    console.log("Base de Datos mongodb conectada")
-  } catch (e) {
-    console.log(e)
-  }
-}
-CRUD()
-
-const app = express()
-const httpServer = new HttpServer(app)
-const io = new IOServer(httpServer)
-
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(
-  session({
-    store: connectMongo.create({
-      mongoUrl: `mongodb+srv://${config.User}:${config.password}@cluster0.qku2u.mongodb.net/sessions?retryWrites=true&w=majority`,
-      mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
-      ttl: 600
-    }),
-    secret: 'secreto',
-    resave: true,
-    saveUnitialized: true,
-  })
-)
-app.use(passport.initialize())
-app.use(passport.session())
-app.set('view engine', 'ejs')
-
-passport.use('login', new LocalStrategy((username, password, done) => {
-  let usuario = modelUser.find({ username: username }, function (err, docs) {
-    if (docs.length == 0) {
-      return done(null, false)
-    } else {
-      bcrypt.compare(password, docs[0].password, function (err, result) {
-        if (result == true) {
-          const user = { username: docs[0].username }
-          return done(null, user)
-        } else {
-          return done(null, false)
-        }
-      })
-    }
-  })
-})
-)
-
-passport.use('registro', new LocalStrategy({ passReqToCallback: true }, (req, username, password, done) => {
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash(password, salt, function (err, hash) {
-      let usuarioscont = modelUser.find({ username: username }, function (err, docs) {
-        if (docs.length == 0) {
-          async function send() {
-            await new modelUser({ username: username, password: hash }).save()
-            return done(null, { username: username })
-          }
-          send()
-        } else {
-          return done(null, false)
-        }
-      })
-    });
-  });
-}
-))
-
-passport.serializeUser((usuario, done) => {
-  done(null, usuario.username)
-})
-
-passport.deserializeUser((username, done) => {
-  let usuario = modelUser.find({ username: username }, function (err, docs) {
-    const user = { username: docs[0].username, password: docs[0].password }
-    done(null, user)
-  })
-})
-
-const optionsMini = { default: { puerto: '8080' } }
+const optionsMini = { default: { puerto: '8080', modo: 'FORK' } }
 const args = parseArgs(process.argv, optionsMini)
-
+const modo = args.modo
 const PORT = args.puerto
-const connectedServer = httpServer.listen(PORT, () => {
-  console.log(`Servidor Http con Websockets escuchando en el puerto ${connectedServer.address().port}`)
-})
-connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
 
-// knex.schema
-//   .createTable('productos', (table) => {
-//     table.string('nombre', 20).notNullable()
-//     table.integer('precio', 7)
-//     table.string('urlfoto', 500)
-//   })
-//   .then(() => console.log('table creada'))
-//   .catch((err) => console.log(err))
-
-app.get('/Fallo', (req, res) => {
-  res.render('login-error.ejs')
-})
-
-app.get('/Fallo2', (req, res) => {
-  res.render('Regis-error.ejs')
-})
-
-app.get('/login', (req, res) => {
-  req.logOut()
-  res.render('formularioLog.ejs')
-})
-
-app.post('/login', passport.authenticate('login',
-  {
-    successRedirect: 'http://localhost:8080',
-    failureRedirect: '/Fallo'
+if (modo == 'CLUSTER' && cluster.isPrimary) {
+  const cantNucleos = os.cpus().length
+  console.log(`numeros de cpus = ${cantNucleos}`)
+  for (let i = 0; i < cantNucleos; i++) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker) => {
+    console.log(`El proceso de id: ${worker.process.pid}, murio`)
+    cluster.fork()
   })
-)
+} else {
 
-app.get('/registro', (req, res) => {
-  res.render('formularioRegist.ejs')
-})
 
-app.post('/registro', passport.authenticate('registro',
-  {
-    successRedirect: '/login',
-    failureRedirect: '/Fallo2'
-  })
-)
-
-app.post('/log-out', (req, res) => {
-  let user = req.user.username
-  res.render('Bye.ejs', { user: user })
-})
-
-app.get('/', (req, res, next) => {
-  if (req.user == undefined) {
-    res.redirect('/login')
-  } else {
-    next()
-  }
-}, (req, res) => {
-  let user = req.user.username
-  res.render('index.ejs', { nombre: user })
-}
-)
-
-app.get('/info', (req, res) =>{
-  let objeto = {
-    ArgumentosDeEntrada: args,
-    SistemaOperativo: os.type(),
-    VersionNodejs: process.version,
-    Rss: process.memoryUsage.rss(),
-    PathEjecucion: process.execPath,
-    PathEjecutable: args._[1]
-  }
-  res.send(objeto)
-})
-
-app.get('/api/randoms', (req, res) => {
-  const forked = fork('./fork.js')
-  let cant = req.query.cant
-  forked.send({ cant: cant })
-  forked.on('message', (numeros) => {
-    res.send(numeros)
-  })
-})
-
-app.get('/api/productos-test', (req, res) => {
-  let productos = []
-  for (let i = 0; i < 5; i++) {
-    let producto = {}
-    producto.Nombre = faker.commerce.product()
-    producto.Precio = faker.commerce.price()
-    producto.UrlFoto = faker.image.imageUrl()
-    productos.push(producto)
-  }
-  res.send(JSON.stringify(productos))
-})
-
-io.on('connection', socket => {
-  async function mensaje() {
-    let Post = await modelPost.find()
-    socket.emit('messages', Post)
-  }
-  mensaje()
-
-  knex
-    .from('productos')
-    .select('*')
-    .then((productos) => {
-      socket.emit('product', productos)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
-  socket.on('new-message', (post) => {
-    post.fecha = moment().format('(DD/MM/YYYY hh:mm:ss a)')
-    const author = new normalizr.schema.Entity('author')
-    const comment = new normalizr.schema.Entity('comment')
-    const fecha = new normalizr.schema.Entity('fecha')
-    const organigrama = new normalizr.schema.Entity('organigrama', [{
-      author: author,
-      text: comment,
-      fecha: fecha
-    }], { idAttribute: 'id' })
-    const autores = new normalizr.schema.Array(organigrama)
-
-    const normalizado = normalizr.normalize(post, autores)
-
-    async function send() {
-      await new modelPost(normalizado).save()
-      let Post = await modelPost.find()
-      console.log(Post)
-      io.sockets.emit('messages', Post)
+  async function CRUD() {
+    try {
+      const URL = "mongodb://127.0.0.1:27017/post"
+      let rta = await mongoose.connect(URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      console.log("Base de Datos mongodb conectada")
+    } catch (e) {
+      console.log(e)
     }
-    send()
+  }
+  CRUD()
+
+  const app = express()
+  const httpServer = new HttpServer(app)
+  const io = new IOServer(httpServer)
+
+  app.use(express.static('public'))
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(
+    session({
+      store: connectMongo.create({
+        mongoUrl: `mongodb+srv://${config.User}:${config.password}@cluster0.qku2u.mongodb.net/sessions?retryWrites=true&w=majority`,
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+        ttl: 600
+      }),
+      secret: 'secreto',
+      resave: true,
+      saveUnitialized: true,
+    })
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.set('view engine', 'ejs')
+
+  passport.use('login', new LocalStrategy((username, password, done) => {
+    let usuario = modelUser.find({ username: username }, function (err, docs) {
+      if (docs.length == 0) {
+        return done(null, false)
+      } else {
+        bcrypt.compare(password, docs[0].password, function (err, result) {
+          if (result == true) {
+            const user = { username: docs[0].username }
+            return done(null, user)
+          } else {
+            return done(null, false)
+          }
+        })
+      }
+    })
+  })
+  )
+
+  passport.use('registro', new LocalStrategy({ passReqToCallback: true }, (req, username, password, done) => {
+    bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        let usuarioscont = modelUser.find({ username: username }, function (err, docs) {
+          if (docs.length == 0) {
+            async function send() {
+              await new modelUser({ username: username, password: hash }).save()
+              return done(null, { username: username })
+            }
+            send()
+          } else {
+            return done(null, false)
+          }
+        })
+      });
+    });
+  }
+  ))
+
+  passport.serializeUser((usuario, done) => {
+    done(null, usuario.username)
   })
 
-  socket.on('new-product', (producto) => {
-    knex('productos')
-      .insert(producto)
-      .then(() => {
-        console.log("producto agregado")
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+  passport.deserializeUser((username, done) => {
+    let usuario = modelUser.find({ username: username }, function (err, docs) {
+      const user = { username: docs[0].username, password: docs[0].password }
+      done(null, user)
+    })
+  })
+
+  const connectedServer = httpServer.listen(PORT, () => {
+    console.log(`Servidor Http con Websockets escuchando en el puerto ${connectedServer.address().port}`)
+  })
+  connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
+
+
+  const { RenderFallo, RenderFallo2, login, RenderRegis, RenderLogout,
+    RenderPrinc, SendInfo, NumberRandoms, ProductosTest } = require('./Daos/dao')
+
+  // knex.schema
+  //   .createTable('productos', (table) => {
+  //     table.string('nombre', 20).notNullable()
+  //     table.integer('precio', 7)
+  //     table.string('urlfoto', 500)
+  //   })
+  //   .then(() => console.log('table creada'))
+  //   .catch((err) => console.log(err))
+
+  app.get('/Fallo', (req, res) => {
+    RenderFallo(res)
+  })
+
+  app.get('/Fallo2', (req, res) => {
+    RenderFallo2(res)
+  })
+
+  app.get('/login', (req, res) => {
+    login(req, res)
+  })
+
+  app.post('/login', passport.authenticate('login',
+    {
+      successRedirect: 'http://localhost:8080',
+      failureRedirect: '/Fallo'
+    })
+  )
+
+  app.get('/registro', (req, res) => {
+    RenderRegis(res)
+  })
+
+  app.post('/registro', passport.authenticate('registro',
+    {
+      successRedirect: '/login',
+      failureRedirect: '/Fallo2'
+    })
+  )
+
+  app.post('/log-out', (req, res) => {
+    RenderLogout(req, res)
+  })
+
+  app.get('/', (req, res, next) => {
+    if (req.user == undefined) {
+      res.redirect('/login')
+    } else {
+      next()
+    }
+  }, (req, res) => {
+    RenderPrinc(req, res)
+  }
+  )
+
+  app.get('/info', (req, res) => {
+    SendInfo(res)
+  })
+
+  app.get('/api/randoms', (req, res) => {
+    NumberRandoms(req, res)
+  })
+
+  app.get('/api/productos-test', (req, res) => {
+    ProductosTest(res)
+  })
+
+  io.on('connection', socket => {
+    async function mensaje() {
+      let Post = await modelPost.find()
+      socket.emit('messages', Post)
+    }
+    mensaje()
+
     knex
       .from('productos')
       .select('*')
       .then((productos) => {
-        io.sockets.emit('product', productos)
+        socket.emit('product', productos)
       })
       .catch((err) => {
         console.log(err)
       })
+
+    socket.on('new-message', (post) => {
+      post.fecha = moment().format('(DD/MM/YYYY hh:mm:ss a)')
+      const author = new normalizr.schema.Entity('author')
+      const comment = new normalizr.schema.Entity('comment')
+      const fecha = new normalizr.schema.Entity('fecha')
+      const organigrama = new normalizr.schema.Entity('organigrama', [{
+        author: author,
+        text: comment,
+        fecha: fecha
+      }], { idAttribute: 'id' })
+      const autores = new normalizr.schema.Array(organigrama)
+
+      const normalizado = normalizr.normalize(post, autores)
+
+      async function send() {
+        await new modelPost(normalizado).save()
+        let Post = await modelPost.find()
+        console.log(Post)
+        io.sockets.emit('messages', Post)
+      }
+      send()
+    })
+    socket.on('new-product', (producto) => {
+      knex('productos')
+        .insert(producto)
+        .then(() => {
+          console.log("producto agregado")
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      knex
+        .from('productos')
+        .select('*')
+        .then((productos) => {
+          io.sockets.emit('product', productos)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    })
   })
-})
+}
 
