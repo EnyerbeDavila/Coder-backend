8087b72670f778faa0ece919d1a5be69abdd77e0
Enyerbe Davila <Enyerbe.davilalopez@gmail.com>
2022-09-05 16:42:56 -0300

    xxddd

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { Server: IOServer } = require('socket.io')
 const connectMongo = require('connect-mongo')
 const session = require('express-session')
 const parseArgs = require('minimist')
-const { addMessage, RenderChat, RenderChatAdm } = require('./Daos/socketdao')
 const Env = require('./env')
 const routerPrinc = require('./router/routerPrinc')
+const { addMessage, RenderChat, RenderChatAdm } = require('./Daos/socketdao')
 
 const optionsMini = { default: { puerto: '8080' } }
 const args = parseArgs(process.argv, optionsMini)
